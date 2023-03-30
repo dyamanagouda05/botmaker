8cc6ea363fe2e3e1cded4ff80f5241c77aefb498
dyamanagouda05 <124523245+dyamanagouda05@users.noreply.github.com>
2023-03-31 01:27:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,10 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://DB:123@cluster0.aur6qq2.mongodb.net/?retryWrites=true&w=majority
+
+")
+DATABASE_NAME = environ.get('DATABASE_NAME', "DB")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
